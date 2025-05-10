039db76805974d5ff1f17135be9ae7102b9fb927
Bharat Jain <bharat.jain@plaksha.edu.in>
2025-05-10 20:02:30 +0530

    first commit

---
+++ game.py
@@ -0,0 +1,18 @@
+'''
+This is the main game simulator for Monopoly. 
+Given the simulator, we have a state space that needs to be moved
+
+Utils: 
+- Dice (2 dice, 1-6 - total will be used as the next move)
+- Make into one 
+
+Elements: 
+- Player 
+- Bank 
+- Community Cards
+- Chance Cards 
+
+Game: 
+- State (Properties and their orders)
+- 
+'''
